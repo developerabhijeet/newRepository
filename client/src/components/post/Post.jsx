29458cb4874c8941a770c737818fb24628c91f2f
Abhijeet Tiwari <abtiwari@bestpeers.com>
2021-04-09 18:27:28 +0530

    like, unlike&comment changed to thunk, improved ui

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,11 +1,9 @@
-import React, { Component, setState, useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { MDBCol, MDBInput } from "mdbreact";
 import {
   Container, Form,
-  Button, Input, Alert
-} from 'reactstrap';
+  Button, Input, Alert} from 'reactstrap';
 import { useSelector, useDispatch } from 'react-redux';
-import { postReducer } from '../../redux/reducers/userAuthReducer';
 import { postAction } from '../../redux/actions/users/userActions';
 const Post = () => {
   const [post, setPost] = useState('');
@@ -25,13 +23,16 @@ const Post = () => {
   const state = useSelector(state => {
     return state.userLogin;
   });
-  const { loading, userInfo, error } = state;
+  const { loading, userInfo, postInfo, error } = state;
   const username = state.userInfo.user.name;
   const name = username;
+ 
+
   console.log(name)
   const onSubmit = e => {
     e.preventDefault();
-    if (name == '' || post == '' || domain == '') {
+    if (name === '' || post === '' || domain === '') {
+
       setVisible(true);
       setNameError(false);
       setSuccessPost(false);
@@ -50,6 +51,9 @@ const Post = () => {
   return (
     <div>
       <Container className="signup">
+    
+       
+      
         <Alert color="danger" isOpen={visible} toggle={onDismiss}>
           Sorry,You cannot keep the fields Empty!
         </Alert>
@@ -60,6 +64,8 @@ const Post = () => {
           OOPS! You are not allowed to modify this Field
         </Alert>
         <h2>Post Your Problems</h2>
+
+       
         <Form className="form" onSubmit={onSubmit}>
           <MDBCol md="6" className="post-text-center">
             <MDBInput type="textarea"
